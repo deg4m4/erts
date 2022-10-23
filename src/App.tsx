554a3e537ecb9017456e070835678de8881c1d7d
dparthka <parthka.2005@gmail.com>
2022-10-23 19:04:14 +0530

    remove all

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
-/* const { ipcRenderer } = window.require("electron") */
 
 function App() {
-
-/*   const sendData = () => {
-    ipcRenderer.send("setpos", JSON.stringify({
-      name: "parth"
-    }));
-  } */
 
   return (
     <div className="App">
 
-      <button /* onClick={sendData} */ id="qwe">Send</button>
+    
     </div>
   );
 }
